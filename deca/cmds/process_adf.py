e7355001bfbfb5b4289af9825e3ad40f2ca89968
Krys Kamieniecki <krys@kamieniecki.com>
2020-03-24 21:48:59 -0400

    Fixed issue with process_adf and process_rtpc needing the data base because they want to lookup hash strings. With no data base it doesnt translate hash strings

--- deca/cmds/process_adf.py
+++ deca/cmds/process_adf.py
@@ -1,24 +1,17 @@
 import sys
-import os
 from deca.file import ArchiveFile
 from deca.ff_adf import Adf
 
 
-if len(sys.argv) < 2:
-    in_file = '/home/krys/prj/work/gzb/extracted/models/manmade/props/interior/civilian_01_answering_machine.hrmeshc'
-    in_file = '/home/krys/prj/work/gzb/extracted/models/manmade/props/interior/civilian_01_answering_machine.modelc'
-    in_file = '/home/krys/prj/work/gzb/extracted/graphs/check_region_difficulty.graphc'
-    in_file = '/home/krys/prj/work/gzb/extracted/graphs/check_region_difficulty.graphc'
-    in_file = '/home/krys/prj/work/gzb/extracted/gdc/global.gdcc'
-    in_file = '/home/krys/prj/work/gz/extracted/environment/base.environc'
-    in_file = '/home/krys/prj/work/gz/extracted/graphs/car_cabinlight_logic.graphc'
-else:
-    in_file = sys.argv[1]
+class FakeVfs:
+    def hash_string_match(self, hash32=None, hash48=None, hash64=None):
+        return []
 
-file_sz = os.stat(in_file).st_size
+
+in_file = sys.argv[1]
 
 obj = Adf()
 with ArchiveFile(open(in_file, 'rb')) as f:
     obj.deserialize(f)
 
-print(obj.dump_to_string())
+print(obj.dump_to_string(FakeVfs()))
--- deca/cmds/process_rtpc.py
+++ deca/cmds/process_rtpc.py
@@ -1,17 +1,16 @@
 import sys
-import os
 from deca.ff_rtpc import Rtpc
 
 
-if len(sys.argv) < 2:
-    in_file = '/home/krys/prj/work/jc4/__CACHE__/archives_win64/boot_patch/game0.arc/DED58CCC.dat'
-else:
-    in_file = sys.argv[1]
+class FakeVfs:
+    def hash_string_match(self, hash32=None, hash48=None, hash64=None):
+        return []
 
-file_sz = os.stat(in_file).st_size
+
+in_file = sys.argv[1]
 
 with open(in_file, 'rb') as f:
     rtpc = Rtpc()
     rtpc.deserialize(f)
 
-print(rtpc.dump_to_string())
+print(rtpc.dump_to_string(FakeVfs()))

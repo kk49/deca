5bcdd7252784d4860692b42b8e64f507a5c26044
Krys Kamieniecki <krys@kamieniecki.com>
2020-07-04 20:32:22 -0400

    More appveyor/discord

--- appveyor/send.ps1
+++ appveyor/send.ps1
@@ -57,7 +57,7 @@ else {
 }
 
 $CHANGE_LOG_LINES="$(python ./appveyor/get_dev_changelog.py CHANGELOG.md)" -replace "`"", "'"
-$OFS = "`r`n"
+$CHANGE_LOG_LINES=$CHANGE_LOG_LINES -join "`r`n"
 
 Write-Output $CHANGE_LOG_LINES
 
